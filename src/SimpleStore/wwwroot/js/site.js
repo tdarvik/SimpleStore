47e14f56787b64a20b32e8e952a0245275970620
Darvik <thomas.darvik@norges-bank.no>
2025-05-26 16:58:02 +0200

    Bytter ut single quote med dobbel

--- src/SimpleStore/wwwroot/js/site.js
+++ src/SimpleStore/wwwroot/js/site.js
@@ -1,45 +1,45 @@
-﻿document.addEventListener('DOMContentLoaded', () => {
+﻿document.addEventListener("DOMContentLoaded", () => {
     fetchItems();
 
-    const form = document.getElementById('addItemForm');
-    form.addEventListener('submit', function (event) {
+    const form = document.getElementById("addItemForm");
+    form.addEventListener("submit", function (event) {
         event.preventDefault();
         addItem();
     });
 });
 
 function fetchItems() {
-    fetch('/api/store')
+    fetch("/api/store")
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error("Network response was not ok");
             }
             return response.json();
         })
         .then(data => {
-            const tableBody = document.getElementById('itemTableBody');
-            tableBody.innerHTML = '';
+            const tableBody = document.getElementById("itemTableBody");
+            tableBody.innerHTML = "";
             data.forEach(item => {
-                const row = document.createElement('tr');
+                const row = document.createElement("tr");
 
                 row.innerHTML = `
                 <td>${item.name}</td>
                 <td>${item.description}</td>
                 <td>${item.price},-</td>
-                <td class="actions"><button onclick="deleteItem('${item.id}')">Delete</button></td>
+                <td class="actions"><button onclick="deleteItem("${item.id}")">Delete</button></td>
                 `;
 
                 tableBody.appendChild(row);
             });
         })
         .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
+            console.error("There was a problem with the fetch operation:", error);
         });
 }
 
 function deleteItem(id) {
     fetch(`/api/store/${id}`, {
-        method: 'DELETE'
+        method: "DELETE"
     }).then(response => {
         if (!response.ok) return;
         fetchItems();
@@ -49,11 +49,11 @@ function deleteItem(id) {
 function addItem() {
     if (!validInput()) return;
     const editorValues = getEditorValues();
-    fetch('/api/store', {
-        method: 'POST',
+    fetch("/api/store", {
+        method: "POST",
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            "Accept": "application/json",
+            "Content-Type": "application/json"
         },
         body: JSON.stringify({
             name: editorValues[0].value,
